b95220c5a9d3e1c78f5f81436356180558addc17
Olivia <osa12@sfu.ca>
2023-03-24 09:34:59 -0700

    responsiveness

--- js/script.js
+++ js/script.js
@@ -1,31 +1,45 @@
 
+function customCursor(){
+  var cursor = document.querySelector('.cursor');
 
-var cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', function (e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
-
-
-
-var links = document.querySelectorAll('a');
-
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener('mouseover', () => {
+  document.addEventListener('mousemove', function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+  });
+  
+  var links = document.querySelectorAll('a');
+  
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', () => {
+      cursor.classList.add('cursor-expand');
+    });
+  
+    links[i].addEventListener('mouseleave', () => {
+      cursor.classList.remove('cursor-expand');
+    });
+  }
+  
+  document.addEventListener('mousedown', function () {
     cursor.classList.add('cursor-expand');
   });
-
-  links[i].addEventListener('mouseleave', () => {
+  
+  document.addEventListener('mouseup', function () {
     cursor.classList.remove('cursor-expand');
   });
 }
 
-document.addEventListener('mousedown', function () {
-  cursor.classList.add('cursor-expand');
-});
 
-document.addEventListener('mouseup', function () {
-  cursor.classList.remove('cursor-expand');
-});
+
+if (window.innerWidth > 768) {
+  customCursor();
+}
+
+
+
+// if (window.innerWidth <= 768) {
+//   document.getElementsByTagName('BODY')[0].style.cursor = 'default'
+// } else {
+//   customCursor();
+// }
+
