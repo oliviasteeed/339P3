16a54b3bbbfecf7c8dc997e9a0fb750062fd736c
Olivia <osa12@sfu.ca>
2023-03-24 07:52:29 -0700

    adding scss and custom cursor + style edits

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
-var cursor = document.getElementsByClassName('rounded');
 
-var moveCursor = function(e) {
-    var mouseY = e.clientY;
-    var mouseX = e.clientX;
-     
-    cursorRounded.style.transform = `translated(${mouseX}px, ${mouseY}px, 0)`;
-  }
-  
-  window.addEventListener('mousemove', moveCursor)
+
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+
+
+var links = document.querySelectorAll('a');
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('mouseover', () => {
+    cursor.classList.add('cursor-expand');
+  });
+
+  links[i].addEventListener('mouseleave', () => {
+    cursor.classList.remove('cursor-expand');
+  });
+}
+
+document.addEventListener('mousedown', function () {
+  cursor.classList.add('cursor-expand');
+});
+
+document.addEventListener('mouseup', function () {
+  cursor.classList.remove('cursor-expand');
+});
